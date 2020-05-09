70d884a674f3e569ef721ed53b2f8902195895f8
Ville M. Vainio <ville.vainio@basware.com>
2020-05-09 16:58:36 +0300

    renames

--- bin/lambda.ts
+++ bin/lambda.ts
@@ -3,8 +3,8 @@ import "source-map-support/register"
 import cdk = require("@aws-cdk/core")
 import { DropZoneStack } from "../lib/lambda-api-stack"
 
-export const lambdaApiStackName = "CDKExampleLambdaApiStack"
-export const lambdaFunctionName = "CDKExampleWidgetStoreFunction"
+export const lambdaApiStackName = "DropzoneStack"
+export const lambdaFunctionName = "DropzoneFunction"
 
 const app = new cdk.App()
 new DropZoneStack(app, lambdaApiStackName, {
